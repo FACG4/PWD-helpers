dcd6827bbe07ac60fce038e34c3386281cca9139
Balsam-Faysal <blsam.2332016@gmail.com>
2018-06-05 11:50:12 +0300

    first file structrue

---
+++ src/model/db_build.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const dbConnection = require('./db_connection');
+
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+
+dbConnection.query(sql, (err, res) => {
+  if (err) {
+    throw new Error(err);
+  } else {
+    console.log(`Database Have been Built Successfuly in ${process.env.NODE_ENV || 'DEVELOPMENT'} mode`);
+  }
+});
