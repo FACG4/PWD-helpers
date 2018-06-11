759a7880776ba45495bd257b56a7b92c56fa37c3
sallamtanna <sallamtanna2015@hotmail.com>
2018-06-11 12:35:30 +0300

    solve selector function error

--- public/js/domSectionB.js
+++ public/js/domSectionB.js
@@ -6,43 +6,44 @@ var b5 = selectorById('B.5');
 var b6 = selectorById('B.6');
 var b7 = selectorById('B.7');
 var textArea = selectorById('sectionBTextarea');
-var analysis = selectorById('analysis');
- 
-const solution1 = "Go to hell";
-
-
 
 b1.addEventListener('change', () => {
   if (b1.checked) {
-    analysis.textContent = solution1;
-    console.log(b1.value);
+     console.log(b1.value);
+     analysis.textContent = 'Sallam';
   }
 })
+
 b2.addEventListener('change', () => {
   if (b2.checked) {
     console.log(b2.value);
   }
 })
+
 b3.addEventListener('change', () => {
   if (b3.checked) {
     console.log(b3.value);
   }
 })
+
 b4.addEventListener('change', () => {
   if (b4.checked) {
     console.log(b4.value);
   }
 })
+
 b5.addEventListener('change', () => {
   if (b5.checked) {
     console.log(b5.value);
   }
 })
+
 b6.addEventListener('change', () => {
   if (b6.checked) {
     console.log(b6.value);
   }
 })
+
 b7.addEventListener('change', () => {
   if (b7.checked) {
     console.log(b7.value);
