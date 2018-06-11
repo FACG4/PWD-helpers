23dd2052fe55579b6fbd9a5121d4288415de38a0
sallamtanna <sallamtanna2015@hotmail.com>
2018-06-11 09:45:14 +0300

    adding dom files

---
+++ public/js/domSectionC.js
@@ -0,0 +1,16 @@
+const result = document.getElementById('analysis')
+
+const c1 = document.getElementById('C.1');
+const c2= document.getElementById('C.2');
+const c3= document.getElementById('C.3');
+const c4= document.getElementById('C.4');
+const c5= document.getElementById('C.5');
+const c6= document.getElementById('C.6');
+const c7_1_1= document.getElementById('C.7.1.1');
+const c7_1_2= document.getElementById('C.7.1.2');
+const c7_1_3= document.getElementById('C.7.1.3');
+const c7_1_4= document.getElementById('C.7.1.4');
+const c7_1_5= document.getElementById('C.7.1.5');
+const c7_1_6= document.getElementById('C.7.1.6');
+const c7_2_1= document.getElementById('C.7.2.1');
+const c7_2_2= document.getElementById('C.7.2.2');
