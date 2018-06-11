a268defaee875fedc8c10233c10b11c648cbe350
sallamtanna <sallamtanna2015@hotmail.com>
2018-06-11 10:29:28 +0300

    new dom files

--- public/js/domSectionC.js
+++ public/js/domSectionC.js
@@ -14,3 +14,108 @@ const c7_1_5= document.getElementById('C.7.1.5');
 const c7_1_6= document.getElementById('C.7.1.6');
 const c7_2_1= document.getElementById('C.7.2.1');
 const c7_2_2= document.getElementById('C.7.2.2');
+const c7_2_3= document.getElementById('C.7.2.3');
+const c7_2_4= document.getElementById('C.7.2.4');
+const c7_2_5= document.getElementById('C.7.2.5');
+
+c1.addEventListener('change' , ()=>{
+  if(c1.checked){
+    console.log(c1.value);
+  }
+})
+
+c2.addEventListener('change' , ()=>{
+  if(c2.checked){
+    console.log(c2.value);
+  }
+})
+
+c3.addEventListener('change' , ()=>{
+  if(c3.checked){
+    console.log(c3.value);
+  }
+})
+
+c4.addEventListener('change' , ()=>{
+  if(c4.checked){
+    console.log(c4.value);
+  }
+})
+
+c5.addEventListener('change' , ()=>{
+  if(c5.checked){
+    console.log(c5.value);
+  }
+})
+
+c6.addEventListener('change' , ()=>{
+  if(c6.checked){
+    console.log(c6.value);
+  }
+})
+
+c7_1_1.addEventListener('change' , ()=>{
+  if(c7_1_1.checked){
+    console.log(c7_1_1.value);
+  }
+})
+
+c7_1_2.addEventListener('change' , ()=>{
+  if(c7_1_2.checked){
+    console.log(c7_1_2.value);
+  }
+})
+
+c7_1_3.addEventListener('change' , ()=>{
+  if(c7_1_3.checked){
+    console.log(c7_1_3.value);
+  }
+})
+
+c7_1_4.addEventListener('change' , ()=>{
+  if(c7_1_4.checked){
+    console.log(c7_1_4.value);
+  }
+})
+
+c7_1_5.addEventListener('change' , ()=>{
+  if(c7_1_5.checked){
+    console.log(c7_1_5.value);
+  }
+})
+
+c7_1_6.addEventListener('change' , ()=>{
+  if(c7_1_6.checked){
+    console.log(c7_1_6.value);
+  }
+})
+
+c7_2_1.addEventListener('change' , ()=>{
+  if(c7_2_1.checked){
+    console.log(c7_2_1.value);
+  }
+})
+
+c7_2_2.addEventListener('change' , ()=>{
+  if(c7_2_2.checked){
+    console.log(c7_2_2.value);
+  }
+})
+
+c7_2_3.addEventListener('change' , ()=>{
+  if(c7_2_3.checked){
+    console.log(c7_2_3.value);
+  }
+})
+
+c7_2_4.addEventListener('change' , ()=>{
+  if(c7_2_4.checked){
+    console.log(c7_2_4.value);
+  }
+})
+
+c7_2_5.addEventListener('change' , ()=>{
+  if(c7_2_5.checked){
+    console.log(c7_2_5.value);
+  }
+})
---
+++ public/js/domSectionD.js
@@ -0,0 +1,51 @@
+const d1= document.getElementById('D.1');
+const d2= document.getElementById('D.2');
+const d3= document.getElementById('D.3');
+const d4= document.getElementById('D.4');
+const d5= document.getElementById('D.5');
+const d6= document.getElementById('D.6');
+const d7= document.getElementById('D.7');
+const d8= document.getElementById('D.8');
+const d9= document.getElementById('D.9');
+const d10= document.getElementById('D.10');
+const d11= document.getElementById('D.11');
+const d12= document.getElementById('D.12');
+const d13= document.getElementById('D.13');
+const d14= document.getElementById('D.14');
+const d15= document.getElementById('D.15');
+
+d1.addEventListener('change' , ()=>{
+  if(d1.checked){
+    console.log(d1.value);
+  }
+})
+
+d2.addEventListener('change' , ()=>{
+  if(d2.checked){
+    console.log(d2.value);
+  }
+})
+
+d3.addEventListener('change' , ()=>{
+  if(d3.checked){
+    console.log(d3.value);
+  }
+})
+
+d4.addEventListener('change' , ()=>{
+  if(d4.checked){
+    console.log(d4.value);
+  }
+})
+
+d5.addEventListener('change' , ()=>{
+  if(d5.checked){
+    console.log(d5.value);
+  }
+})
+
+d6.addEventListener('change' , ()=>{
+  if(d6.checked){
+    console.log(d6.value);
+  }
+})
