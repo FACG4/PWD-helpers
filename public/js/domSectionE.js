cc670fdb77801e7103ed3f53129e3f4055636273
sallamtanna <sallamtanna2015@hotmail.com>
2018-06-11 11:49:32 +0300

    add new dommmmmmm files

--- public/js/domSectionE.js
+++ public/js/domSectionE.js
@@ -1,23 +1,21 @@
-const result = document.getElementById('analysis')
+ const e1 = selectorById('E.1');
+ const e2 = selectorById('E.2');
+ const e3 = selectorById('E.3');
 
-const e1 = document.getElementById('E.1');
-const e2 = document.getElementById('E.2');
-const e3 = document.getElementById('E.3');
+ e1.addEventListener('change', () => {
+   if (e1.checked) {
+     console.log(e1.value);
+   }
+ })
 
-e1.addEventListener('change' , ()=>{
-  if(e1.checked){
-    console.log(e1.value);
-  }
-})
+ e2.addEventListener('change', () => {
+   if (e2.checked) {
+     console.log(e2.value);
+   }
+ })
 
-e2.addEventListener('change' , ()=>{
-  if(e2.checked){
-    console.log(e2.value);
-  }
-})
-
-e3.addEventListener('change' , ()=>{
-  if(e3.checked){
-    console.log(e3.value);
-  }
-})
+ e3.addEventListener('change', () => {
+   if (e3.checked) {
+     console.log(e3.value);
+   }
+ })
