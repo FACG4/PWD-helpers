63e44360de41b8d8736dad266c4e0d2009066ad4
IsraaMoqbel <isramm94@gmail.com>
2018-06-12 14:26:13 +0300

    adding analysis

--- src/controllers/analysis.js
+++ src/controllers/analysis.js
@@ -1,7 +1,43 @@
-exports.post = (req, res)=>{
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+var util = require('util');
+const formidable = require('formidable');
+const http = require('http');
+const util = require('util');
 
-console.log(req.body);
+exports.post = (req, res) => {
+  let solutions = [];
+  let solutinA1 = ' you have to do something for the entrance';
+  if (req.body.A1 === 'No') {
+    solutions.push('Solution for A1' + solutinA1);
+  }
+  if (req.body.A2 === 'No') {
+    solutions.push('Solution for A2' + solutinA1);
+  }
+  res.render('analysis', {
+    solutions
+  });
 
-
-  res.render('analysis');
 }
+
+// exports.get = (req, res)=>{
+
+//   res.render('fileupload');
+//for uploading images!!!!!!
+//   var form = new formidable.IncomingForm();
+// form.encoding = 'utf-8';
+// form.uploadDir = __dirname + "/images";
+// form.keepExtensions = true;
+//   form.parse(req, function (err, fields, files) {
+// var oldpath = files.filetoupload.path;
+// var newpath = '/home/israa/PWD-helpers/src/controllers/images/' + files.filetoupload.name;
+// fs.rename(oldpath, newpath, function (err) {
+//       if (err) throw err;
+//       res.render('fileupload')
+//     });
+// });
+// res.send(req.body);
+// if(req.body['B.2']==='No'){
+// }
+// }
